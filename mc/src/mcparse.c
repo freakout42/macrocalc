16d641745042ff9ea4210477b1c255b14179163f
Axel K. Reinhold <github@freakout.de>
2022-12-05 21:18:50 +0100

    dateformat

--- mc/src/mcparse.c
+++ mc/src/mcparse.c
@@ -103,7 +103,10 @@ yydebug = 1;
 yybegin();
 yyparse();
 if (cpunit(pc) && strspn(cpunit(pc), " ")==strlen(cpunit(pc))) cpunit(pc) = NULL;
-if (cptype(pc) == DATETYPE) cptype(pc) = CONSTANT;
+if (cptype(pc) == DATETYPE) {
+  cptype(pc) = CONSTANT;
+  if (cpfor(pc) == 0) cpfor(pc) = SPECIAL|DATE;
+}
 yyclrbuf();
 
 #ifdef LOTUS
