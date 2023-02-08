c397e8b94de4e020dca343f76213e826a329e271
Axel K. Reinhold <github@freakout.de>
2023-02-08 20:15:16 +0100

    ldd4rpath

--- mc/src/mcuser.mk
+++ mc/src/mcuser.mk
@@ -1,4 +1,4 @@
-# $Id: mcuser.mk,v 1.20 2017/07/11 13:17:25 axel Exp $
+# mcuser.mk 1.20 2017/07/11 13:17:25 axel
 
 CC=$Cc
 CFLAGS=$Cflag
@@ -15,7 +15,7 @@ install: all
 	mv $(PROGS) ../../bin
 
 mc:	mc.o mcpary.o mc_kern.o mc_io.o libmcf.a mcftab.o mcuser.o
-	$(CC) $(CFLAGS) mc.o mcpary.o mc_kern.o mc_io.o mcftab.o mcuser.o libmcf.a $(ARXLIB) $(CURLIB) $(LEXLIB) -lm -o $@
+	$(CC) $(CFLAGS) $(CLDFLAGS) mc.o mcpary.o mc_kern.o mc_io.o mcftab.o mcuser.o libmcf.a $(ARXLIB) $(CURLIB) $(LEXLIB) -lm -o $@
 
 mc2wks:	mc2wks.o mc2pary.o mc_kern.o libmcf.a mcftab.o mcuser.o
 	$(CC) $(CFLAGS) mc2wks.o mc2pary.o mc_kern.o mcftab.o mcuser.o libmcf.a $(ARXLIB) $(LEXLIB) -lm -o $@
