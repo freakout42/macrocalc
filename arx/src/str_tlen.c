48df6c0de583529aecf2ddf9c6f13edf3230cea5
Axel K. Reinhold <github@freakout.de>
2025-06-21 10:40:00 +0200

    utf8len

--- arx/src/str_tlen.c
+++ arx/src/str_tlen.c
@@ -1,11 +1,34 @@
 /* $Id: str_tlen.c,v 1.1 2015/02/04 11:34:13 axel Exp $
  * length without trailing whitespace
+ mbstowcs
  */
 
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <arx_def.h>
 #include <str_def.h>
+
+int *str_offi(char *s) {
+/*
+    size_t count = 0;
+    while (*s) {
+        count += (*s++ & 0xC0) != 0x80;
+    }
+    return count;
+ */
+int *offs;
+int *p;
+char *q;
+int i, j;
+j = strlen(s);
+if (j >= MAXSTRING || ((offs = malloc(sizeof(int *)*j)) == NULL)) return NULL;
+for (p = offs, q = s; *q; p++) {
+  i = 1;
+}
+return p;
+}
 
 int str_tlen (char *s)
 {
